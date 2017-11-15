87ef16e1274de2b269ca0b0e8549279ee7a451ce
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-15 17:35:14 -0500

    Add AJAX request to append candidate list items, from JSON response

--- election.js
+++ election.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+  var candidateList = document.querySelector('#candidate-list');
   // Imagination!
-
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done( function(responseData) {
+    console.log('Request success!');
+    responseData.candidates.forEach(function(candidate) {
+      var newItem = document.createElement('li');
+      newItem.innerHTML = candidate.name + ", votes: " + candidate.votes;
+      candidateList.append(newItem);
+    })
+  });
 });
